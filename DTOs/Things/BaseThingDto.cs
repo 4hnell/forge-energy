e27d57e61754de313fa4baadb9b99a35a49a8dc7
FredrikAhnell <4hnell@gmail.com>
2026-03-25 17:35:45 +0100

    fixed

--- DTOs/Things/BaseThingDto.cs
+++ DTOs/Things/BaseThingDto.cs
@@ -1,7 +1,3 @@
 namespace forge_energy.DTOs.Things;
 
-public abstract class BaseThingDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
+public abstract class BaseThingDto { }
