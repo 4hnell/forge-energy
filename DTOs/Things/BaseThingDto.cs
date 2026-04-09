6d595374cc50f0c6d4092c778519cf8f58c2ab1d
Fredrik Ahnell <4hnell@gmail.com>
2026-04-09 11:11:47 +0200

    major fix

--- DTOs/Things/BaseThingDto.cs
+++
@@ -1,7 +0,0 @@
-namespace forge_energy.DTOs.Things;
-
-public abstract class BaseThingDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
--- Entities/Thing.cs
+++
@@ -1,7 +0,0 @@
-namespace forge_energy.Entities;
-
-public class Thing
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
